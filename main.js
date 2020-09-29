e22655cab1468ab5d14e0d4f72231c3967a726b2
kkr <kanhaiyakumar1995@outlook.com>
2020-09-29 17:32:56 +0530

    mainjs modified

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
-var Objpeople = 
-[
-		{
-
-		 	email:"kanhaiyakumar@gmail.com",
-		 	password:"kanhaiya"
-		},
+var Objpeople = [
+	{
+		email:"kanhaiyakumar@gmail.com",
+		password:"kanhaiya"
+	},
 		
-		{
-			email:"kanhaiyakumar123@gmail.com",
-		 	password:"kanhaiya123"
-
-		}
-
+	{
+		email:"kanhaiyakumar123@gmail.com",
+		password:"kanhaiya123"
+	}
 ]
 
-
-
- function getInfo() 
-	 	{
-
-			var email = document.getElementById("email").value
-			var password = document.getElementById("password").value
-			console.log("your email id is " + email + " and your password is " + password)
-
-
-			for(var i=0; i<Objpeople.length; i++)
+function getInfo() {
+	
+	var email = document.getElementById("email").value
+	var password = document.getElementById("password").value
+	console.log("your email id is " + email + " and your password is " + password)
+		
+		for(var i=0; i<Objpeople.length; i++)
 			{
-
-				 if(email==Objpeople[i].email && password==Objpeople[i].password)
+				if(email==Objpeople[i].email && password==Objpeople[i].password)
 				{
 					console.log(email + "is logedin!!")
 						return
 				}
 			}
-				console.log("incorrect emai id and Password")
-		}
+				console.log("incorrect emai id and Password")		
+}
+	
