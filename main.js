2a2aa278831a4fb01fa895404b47871416126f05
kkr <kanhaiyakumar1995@outlook.com>
2020-09-29 19:10:52 +0530

    js indentation done

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var Objpeople = [{
-
+var Objpeople = [
+	{
 		email:"kanhaiyakumar@gmail.com",
 		password:"kanhaiya"
 	},
@@ -9,19 +9,16 @@ var Objpeople = [{
 	}
 ]
 function getInfo() {
-	
 	var email = document.getElementById("email").value
 	var password = document.getElementById("password").value
 	console.log("your email id is " + email + " and your password is " + password)
 		
-		for(var i=0; i<Objpeople.length; i++)
-			{
-				if(email==Objpeople[i].email && password==Objpeople[i].password)
-				{
-					console.log(email + "is logedin!!")
-						return
-				}
+		for(var i=0; i<Objpeople.length; i++){
+			if(email==Objpeople[i].email && password==Objpeople[i].password){
+				console.log(email + "is logedin!!")
+				return
 			}
-				console.log("incorrect emai id and Password")		
+		}
+		console.log("incorrect emai id and Password")		
 }
 	
